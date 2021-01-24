c1d47676e79f316bcc3dfb3e0b1ca402c447816d
NickoDante <nickodante24@gmail.com>
2021-01-24 16:14:25 -0500

    ** What is GAS ?
    - Activating GAS Plugin
    - Create our own GameplayAbility, Gameplay Effect and AttributeSet

---
+++ Source/Arkde_MultiplayerGAS/Public/GAS_GameplayEffect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GAS_GameplayEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_MULTIPLAYERGAS_API UGAS_GameplayEffect : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
