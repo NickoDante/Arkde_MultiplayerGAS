38a0ef1edba99c58e44664fe8f4405e16dab3b0b
Nicolas Nieto <nnieto@teravisiongames.com>
2021-02-01 08:10:29 -0500

    ** Attribute set.
    - Creating all the enviroment to modify attribute values.

--- Source/Arkde_MultiplayerGAS/Private/GAS_AttributeSet.cpp
+++ Source/Arkde_MultiplayerGAS/Private/GAS_AttributeSet.cpp
@@ -2,4 +2,87 @@
 
 
 #include "GAS_AttributeSet.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+UGAS_AttributeSet::UGAS_AttributeSet()
+{
+	MaxHealth = 100.0f;
+	Health = MaxHealth;
+	HealthRegen = 0.5f;
+
+	MaxMana = 100.0f;
+	Mana = MaxMana;
+	ManaRegen = 2.0f;
+
+	MaxStamina = 100.0f;
+	Stamina = MaxStamina;
+	StaminaRegen = 1.0f;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+void UGAS_AttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		AdjustAttributeForMaxChange(Health, MaxHealth, NewValue, GetHealthAttribute());
+	}
+	else if (Attribute == GetMaxManaAttribute())
+	{
+		AdjustAttributeForMaxChange(Mana, MaxMana, NewValue, GetManaAttribute());
+	}
+	else if (Attribute == GetMaxStaminaAttribute())
+	{
+		AdjustAttributeForMaxChange(Stamina, MaxStamina, NewValue, GetStaminaAttribute());
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+void UGAS_AttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData &Data)
+{
+	if (Data.EvaluatedData.Attribute.GetUProperty() == 
+		FindFieldChecked<FProperty>(UGAS_AttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UGAS_AttributeSet, Health)))
+	{
+		Health.SetCurrentValue(FMath::Clamp(Health.GetCurrentValue(), 0.f, MaxHealth.GetCurrentValue()));
+		Health.SetBaseValue(FMath::Clamp(Health.GetBaseValue(), 0.f, MaxHealth.GetBaseValue()));
+	}
+	else if (Data.EvaluatedData.Attribute.GetUProperty() ==
+		FindFieldChecked<FProperty>(UGAS_AttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UGAS_AttributeSet, Mana)))
+	{
+		Mana.SetCurrentValue(FMath::Clamp(Mana.GetCurrentValue(), 0.f, MaxMana.GetCurrentValue()));
+		Mana.SetBaseValue(FMath::Clamp(Mana.GetBaseValue(), 0.f, MaxMana.GetBaseValue()));
+	}
+	else if (Data.EvaluatedData.Attribute.GetUProperty() ==
+		FindFieldChecked<FProperty>(UGAS_AttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UGAS_AttributeSet, Stamina)))
+	{
+		Stamina.SetCurrentValue(FMath::Clamp(Stamina.GetCurrentValue(), 0.f, MaxStamina.GetCurrentValue()));
+		Stamina.SetBaseValue(FMath::Clamp(Stamina.GetBaseValue(), 0.f, MaxStamina.GetBaseValue()));
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+void UGAS_AttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty)
+{
+	UAbilitySystemComponent* AbilityComponent = GetOwningAbilitySystemComponent(); // Obtain the Ability System Component, no matter the actor.
+
+	if (!IsValid(AbilityComponent))
+	{
+		return;
+	}
+
+	const float CurrentMaxValue = MaxAttribute.GetCurrentValue();
+
+	if (!FMath::IsNearlyEqual(CurrentMaxValue, NewMaxValue)) // We use it to avoid near zero values
+	{
+		const float CurrentValue = AffectedAttribute.GetCurrentValue();
+		const float NewDelta = CurrentMaxValue > 0.f ? (CurrentValue * NewMaxValue / CurrentMaxValue) - CurrentValue : NewMaxValue;
+		AbilityComponent->ApplyModToAttributeUnsafe(AffectedAttributeProperty, EGameplayModOp::Additive, NewDelta);
+	}
+}
--- Source/Arkde_MultiplayerGAS/Public/GAS_AttributeSet.h
+++ Source/Arkde_MultiplayerGAS/Public/GAS_AttributeSet.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "GAS_AttributeSet.generated.h"
+
+// With this macro, every Property created, will have the getter, the setter and the init automatically.
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +21,62 @@ UCLASS()
 class ARKDE_MULTIPLAYERGAS_API UGAS_AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+
+	UGAS_AttributeSet();
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, Health); // Use the definition above to create getter setter and init.
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, MaxHealth);
 	
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData HealthRegen;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, HealthRegen);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+		FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, Mana); // Use the definition above to create getter setter and init.
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+		FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, MaxMana);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+		FGameplayAttributeData ManaRegen;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, ManaRegen);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stamina")
+		FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, Stamina); // Use the definition above to create getter setter and init.
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stamina")
+		FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, MaxStamina);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stamina")
+		FGameplayAttributeData StaminaRegen;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSet, StaminaRegen);
+
+public: 
+
+	// To check Roof Floor of values.
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	// The best way to modify attributes during runtime.
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData &Data) override;
+
+	// Function to modify attributes
+	void AdjustAttributeForMaxChange(
+		FGameplayAttributeData& AffectedAttribute,
+		const FGameplayAttributeData& MaxAttribute,
+		float NewMaxValue,
+		const FGameplayAttribute& AffectedAttributeProperty
+		);
 };
